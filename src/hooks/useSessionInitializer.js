260005aee3aa0a608f9a5b44f7e32c2d78e41c85
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-05-06 14:50:57 +0300

    Fixed typo from previous commit

--- src/hooks/useSessionInitializer.js
+++ src/hooks/useSessionInitializer.js
@@ -7,7 +7,7 @@ const useSessionInitializer = ({ setClicked, setLoading, setTransactionId, setCl
     // Set loading state
     setLoading(true);
 
-    const nonce = crypto.randomUUID();s
+    const nonce = crypto.randomUUID();
 
     try {
         const response = await fetch('https://dev.verifier-backend.eudiw.dev/ui/presentations', {
@@ -17,8 +17,8 @@ const useSessionInitializer = ({ setClicked, setLoading, setTransactionId, setCl
           },
           body: JSON.stringify({  // Basic request body
             "type": "vp_token",
-            "presentation_definition": presentationDefinition,
-            "dcql_query": null,
+            "presentation_definition": null,
+            "dcql_query": dcqlQuery,
             "nonce": nonce,
             "response_mode": "direct_post",
             "jar_mode": "by_reference",
